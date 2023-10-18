f330019c70fe7e9b7bf17602e722599481743c17
nikcpatil <92070128+nikcpatil@users.noreply.github.com>
2023-10-18 14:42:18 +0530

    WrapperClass

---
+++ WrapperClass.java
@@ -0,0 +1,41 @@
+package lab4;
+
+public class WrapperClass {
+	public static void main(String args[]) {
+		//int n = Integer.parseInt()
+		    System.out.println("Integer Max Value: " + Integer.MAX_VALUE);
+	        System.out.println("Integer Min Value: " + Integer.MIN_VALUE);
+	        System.out.println("Integer Size (in bytes): " + Integer.BYTES);
+	        System.out.println("Integer Allocated Memory (in bits): " + Integer.SIZE);
+
+	        
+	        System.out.println("Double Max Value: " + Double.MAX_VALUE);
+	        System.out.println("Double Min Value: " + Double.MIN_VALUE);
+	        System.out.println("Double Size (in bytes): " + Double.BYTES);
+	        System.out.println("Double Allocated Memory (in bits): " + Double.SIZE);
+
+	        
+	        System.out.println("Float Max Value: " + Float.MAX_VALUE);
+	        System.out.println("Float Min Value: " + -Float.MAX_VALUE);
+	        System.out.println("Float Size (in bytes): " + Float.BYTES);
+	        System.out.println("Float Allocated Memory (in bits): " + Float.SIZE);
+
+	        
+	        System.out.println("Short Max Value: " + Short.MAX_VALUE);
+	        System.out.println("Short Min Value: " + Short.MIN_VALUE);
+	        System.out.println("Short Size (in bytes): " + Short.BYTES);
+	        System.out.println("Short Allocated Memory (in bits): " + Short.SIZE);
+
+	       
+	        System.out.println("Character Max Value: " + (int) Character.MAX_VALUE);
+	        System.out.println("Character Min Value: " + (int) Character.MIN_VALUE);
+	        System.out.println("Character Size (in bytes): " + Character.BYTES);
+	        System.out.println("Character Allocated Memory (in bits): " + Character.SIZE);
+
+	       
+	        System.out.println("Long Max Value: " + Long.MAX_VALUE);
+	        System.out.println("Long Min Value: " + Long.MIN_VALUE);
+	        System.out.println("Long Size (in bytes): " + Long.BYTES);
+	        System.out.println("Long Allocated Memory (in bits): " + Long.SIZE);
+	}
+}
