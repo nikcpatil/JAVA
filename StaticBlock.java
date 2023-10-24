9cc019791ea779a44a1a7f33fed53fcb3f2a6deb
nikcpatil <92070128+nikcpatil@users.noreply.github.com>
2023-10-24 22:14:28 +0530

    Static Block

---
+++ StaticBlock.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class StaticBlock {
+	static {//static method
+		System.out.println("Static block");
+	}
+	public static void main(String args[]) {
+		System.out.println("Main method...");
+	}
+}
